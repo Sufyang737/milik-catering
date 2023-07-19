fd92906eb536112e22d2ef13ae3c51caac4426fb
Franco Caputo <caputo.franco125@gmail.com>
2023-07-19 19:37:24 -0300

    actualizacion carrito

--- malik-catering/components/Cart.jsx
+++ malik-catering/components/Cart.jsx
@@ -7,10 +7,10 @@ const Cart = () => {
       <div className='w-full'> 
         <Cartprod/>
       </div>
-      <div className='bg-green-500 w-full h-1/4 flex flex-col justify-evenly items-center'>
+      <div className='bg-green-500 w-full h-1/4 p-4 flex flex-col justify-evenly items-center'>
         <ul className='w-full h-1/2'>
-          <li>Subtotal <span>$0</span></li>
-          <li>Total <span>$0</span></li>
+          <li className='border-b-2 flex justify-between text-2xl py-2'>Subtotal <span>$0</span></li>
+          <li className='flex justify-between text-4xl py-6'>Total <span>$0</span></li>
         </ul>
         <button className='bg-white text-black w-2/5 h-10'>FINALIZAR COMPRA</button>
         <div>
