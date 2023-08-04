be1396d5fcd4dd698134196c755e8d4c26c39c10
Sufyan Ahmed <107362900+Sufyang737@users.noreply.github.com>
2023-08-03 23:40:32 -0300

    mejoras useRef

--- malik-catering/components/Slider.jsx
+++ malik-catering/components/Slider.jsx
@@ -1,14 +1,17 @@
 import React, { useRef, useState } from "react";
 import Product from "./Product";
 
-let sliderCount = 0
-let sliderPercent = 0
-const sliderWrap = useRef();
-console.log(sliderWrap)
 const Slider = () => {
-  // const [Translate, setTranslate] = useState('w-[200%] h-full mx-10 grid grid-flow-col auto-cols-auto gap-1 translate-x-[' + sliderPercent + '%]')
+  const [sliderCount, setSliderCount] = useState(0);
+  const sliderWrap = useRef();
 
-  
+  const handleClickRight = () => {
+    if (sliderCount === 0) {
+      sliderWrap.current.classList.add('-translate-x-[25%]');
+      setSliderCount(1);
+    }
+  };
+
   // const handleClickRight = ()=>{
   //   if (sliderCount > 1 ){
   //     sliderPercent = 0
@@ -26,17 +29,13 @@ const Slider = () => {
   //   sliderCount--
   // }
 
-  const handleClickRight = ()=>{
-    if(sliderCount = 0){
-      sliderWrap.classList.add('-translate-x-[25%]')
-      sliderCount = sliderCount + 1
-    }
-  }
 
-  
   return (
     <div className="h-full overflow-hidden">
-      <div className={'w-[200%] h-full mx-10 grid grid-flow-col auto-cols-auto gap-1 '} ref={sliderWrap}>
+      <div
+        className={'w-[200%] h-full mx-10 grid grid-flow-col auto-cols-auto gap-1 '}
+        ref={sliderWrap}
+      >
         <Product />
         <Product />
         <Product />
@@ -44,37 +43,14 @@ const Slider = () => {
         <Product />
         <Product />
       </div>
-      <button className="absolute bg-white -translate-y-[500%] -translate-x-[100%]"  >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="black"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-12 h-12"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
+      <button className="absolute bg-white -translate-y-[500%] -translate-x-[100%]">
+        {/* Button content */}
       </button>
-      <button className="absolute bg-white -translate-y-[500%] translate-x-[3000%]" onClick={handleClickRight}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="black"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-12 h-12"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
+      <button
+        className="absolute bg-white -translate-y-[500%] translate-x-[3000%]"
+        onClick={handleClickRight}
+      >
+        {/* Button content */}
       </button>
     </div>
   );
